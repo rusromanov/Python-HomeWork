503ee51cf87baaa37259727a9c6c518a274f6026
Ruslan Romanov <rusromanovgit@bk.ru>
2019-12-05 14:06:53 +0300

    Практическое задание №1 Часть 1

---
+++ HomwWork1.py
@@ -0,0 +1,12 @@
+# Coding: utf-8
+
+a, b = 42, 'string'
+
+print(f'Переменная a = {a}')
+print(f'Переменная b = {b}')
+
+integer = int(input('Введите целое число: '))
+string = input('Введите строку: ')
+
+print(f'Целое число: {integer}')
+print(f'Строка: {string}')
