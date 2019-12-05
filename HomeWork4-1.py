df36bc721620258a2cd9bd23a8e6e187210073c2
Ruslan Romanov <rusromanovgit@bk.ru>
2019-12-05 14:07:42 +0300

    Практическое задание №1 Часть 4.1

---
+++ HomeWork4-1.py
@@ -0,0 +1,8 @@
+# Coding: utf-8
+
+# number = int(input('Введите целое положительное число: '))
+number = 123456789987654321
+
+number = str(number)
+
+print(f'Самая большая цифра в числе {number} - это {max(number)}')
